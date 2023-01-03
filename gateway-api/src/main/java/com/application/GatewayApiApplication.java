02cc7714902a65a677d388f83ac9612a0d336477
whd <1263969092@qq.com>
2023-01-03 13:47:03 +0800

    1

--- gateway-api/src/main/java/com/application/GatewayApiApplication.java
+++ gateway-api/src/main/java/com/application/GatewayApiApplication.java
@@ -2,8 +2,10 @@ package com.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApiApplication {
 
 	public static void main(String[] args) {
