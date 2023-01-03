1c6eedf659903d7540fd89e8dbd293ca24c57c84
whd <1263969092@qq.com>
2023-01-03 12:11:26 +0800

    login-service

---
+++ login-service/src/test/java/com/xpu/entity/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpu.entity;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
