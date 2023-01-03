43290d996acf2a2c2925601e76048db6417ebf82
whd <1263969092@qq.com>
2023-01-03 11:00:59 +0800

    测试

---
+++ src/main/java/com/xpu/cos/XpuCosApplication.java
@@ -0,0 +1,13 @@
+package com.xpu.cos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpuCosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpuCosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xpu/cos/XpuCosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpu.cos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpuCosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
