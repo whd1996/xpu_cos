fa3837a94a0bf6042237b287273e9881178e9cbf
whd <1263969092@qq.com>
2023-01-04 17:17:30 +0800

    删除了错误的结构

--- user-manage-service/src/main/java/com/xpu/servive/impl/UserServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.xpu.servive.impl;
-
-import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
-import java.util.List;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.xpu.dao.UserDao;
-import com.xpu.entity.User;
-import com.xpu.service.UserService;
