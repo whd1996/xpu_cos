7b113493eb92e7369949f74734f54bd38646cbd5
whd <1263969092@qq.com>
2023-01-05 00:03:48 +0800

    新增了一个前端接口文档上传文件夹

--- login-service-demo/src/main/java/com/xpu/config/SessionConfiguration.java
+++ login-service-demo/src/main/java/com/xpu/config/SessionConfiguration.java
@@ -1,7 +1,6 @@
 package com.xpu.config;
 
 import com.xpu.interceptor.LoginInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
