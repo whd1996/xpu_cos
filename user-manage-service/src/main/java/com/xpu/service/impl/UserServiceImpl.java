ab8e8e31e03fbffa0ec8597f0be827ced4d9f2a3
whd <1263969092@qq.com>
2023-01-04 22:16:40 +0800

    新增了一个redis的配置类 可使用@Cacheable、@CachePut、@CacheEvict注解在数据查询、更新和删除方法上进行缓存管理

--- user-manage-service/src/main/java/com/xpu/service/impl/UserServiceImpl.java
+++ user-manage-service/src/main/java/com/xpu/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xpu.dao.UserDao;
 import com.xpu.entity.User;
 import com.xpu.service.UserService;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -31,7 +30,7 @@ public class UserServiceImpl extends ServiceImpl<UserDao, User> implements UserS
      * cacheNames: 起一个缓存的命名空间，对应缓存的唯一标识
      * value：缓存结果   key：默认只有一个参数的情况下，key值默认就是方法参数值; 如果没有参数或者多个参数的情况：会使用SimpleKeyGenerate来为生成key
      */
-    @Cacheable(cacheNames = "user", key ="#id" ,unless = "#result==null")
+    @Cacheable(cacheNames = "user",unless ="#result==null")
     @Override
     public User selectUserById(Integer id) {
         User user = userDao.selectById(id);
