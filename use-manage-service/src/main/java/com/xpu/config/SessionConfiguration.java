da7fef5f83ea87e417b74db656b9bd8c50253962
17729164565 <2805817784@qq.com>
2023-01-06 14:55:07 +0800

    提交了用户管理服务 实现了基本的增删改查 提交了用户管理后台api文档

---
+++ use-manage-service/src/main/java/com/xpu/config/SessionConfiguration.java
@@ -0,0 +1,74 @@
+package com.xpu.config;
+
+import com.xpu.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+//登录拦截器配置
+@Configuration
+public class SessionConfiguration implements WebMvcConfigurer {
+	@Resource
+	private LoginInterceptor loginInterceptor;
+
+	@Override
+    public void addInterceptors(InterceptorRegistry registry) {
+		//List<String> addPathList = null;
+		List<String> excludePathList = null;
+		// 登录拦截器
+		InterceptorRegistration login = registry
+				.addInterceptor(this.loginInterceptor);
+		// 设置拦截的路径
+		login.addPathPatterns("/*/*");
+		// 设置例外的路径
+		excludePathList = new ArrayList<>();
+		excludePathList.add("/user/login");
+		excludePathList.add("/login");
+		excludePathList.add("/index");
+		excludePathList.add("/toLogin");
+		excludePathList.add("/user/loginByAnnotation");
+		excludePathList.add("/v2/api-docs-ext");
+		excludePathList.add("/user/foundPwd");
+		excludePathList.add("/user/logout");
+		//excludePathList.add("/user/listUserRole");//测试用
+		excludePathList.add("/user/addUser");
+		excludePathList.add("/css/**");
+		excludePathList.add("/images/**");
+		excludePathList.add("/plugins/**");
+		excludePathList.add("/js/**");
+		excludePathList.add("/data/**");
+		login.excludePathPatterns(excludePathList);
+    }
+	
+	@Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/static/");
+        viewResolver.setSuffix(".html");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/static/js/**").addResourceLocations("classpath:/static/js/");
+		registry.addResourceHandler("/static/css/**").addResourceLocations("classpath:/static/css/");
+		registry.addResourceHandler("/static/data/**").addResourceLocations("classpath:/static/data/");
+		registry.addResourceHandler("/static/images/**").addResourceLocations("classpath:/static/images/");
+		registry.addResourceHandler("/static/plugins/**").addResourceLocations("classpath:/static/plugins/");
+
+		System.out.println("---------------------------");
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+	}
+
+}
---
+++ use-manage-service/src/main/java/com/xpu/entity/User.java
@@ -0,0 +1,55 @@
+package com.xpu.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@ApiModel(value="`user`")
+@Data
+@NoArgsConstructor
+@TableName(value = "`user`")
+public class User implements Serializable {
+    @TableId(value = "id", type = IdType.AUTO)
+    // 使用ApiModelProperty来标注字段属性。
+    @ApiModelProperty(value = "主键id",example = "1")
+    private Integer id;
+
+    @TableField(value = "user_Name")
+    @ApiModelProperty(value="用户名",example = "zhangsan")
+    private String userName;
+
+    @TableField(value = "user_Passward")
+    @ApiModelProperty(value="用户密码",example = "1234")
+    private String userPassward;
+
+    private String nickname;
+
+
+    private String userPhone;
+
+
+    private String address;
+
+
+    private Integer roleId;
+    public static final String COL_ID = "id";
+
+    public static final String COL_USER_NAME = "user_Name";
+
+    public static final String COL_USER_PASSWARD = "user_Passward";
+
+    public static final String COL_NICKNAME = "nickName";
+
+    public static final String COL_USER_PHONE = "user_phone";
+
+    public static final String COL_ADDRESS = "address";
+
+    public static final String COL_ROLE_ID = "role_ID";
+}
