4537f370e27605efd2699cae9a642d970b5d1aa6
张嘉奇 <2071876965@qq.com>
2023-01-06 17:19:18 +0800

    修改了网关配置，重新提交

--- login-service/src/main/java/com/xpu/LoginServiceApplication.java
+++ login-service/src/main/java/com/xpu/LoginServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class LoginServiceApplication {
 	public static void main(String[] args) {
 
+
 		SpringApplication.run(LoginServiceApplication.class, args);
 	}
 }
