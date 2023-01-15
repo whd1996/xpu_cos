77247865f568d7f2e7c2bb05a6c4d632616149f9
lpfggg <210084981@qq.com>
2023-01-15 10:50:34 +0800

    vue代码

---
+++ vue-admin/app/src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex)
+//引入小仓库
+import login from './login/index'
+import zhuce from './zhuce/index'
+export default new Vuex.Store({
+    //实现模块式开发
+    modules:{
+     login,
+     zhuce,
+    }
+})
